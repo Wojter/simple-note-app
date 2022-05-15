448f9dba53c7f561faf1a7b3c8491072169526ed
Wojter <wojtektro2@gmail.com>
2022-05-16 00:08:55 +0200

    simple server works

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const { port } = require('./config')
+const apiRouter = require('./routes/api');
+
+// my routes
+app.use('/', apiRouter);
+
+//server
+app.listen(port, function() {
+    console.log('serwer słucha... http://localhost:' + port);
+})
