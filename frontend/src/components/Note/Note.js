f6e6ca59a6aafa637af63a283c6167c2fa634433
Wojter <wojtektro2@gmail.com>
2022-05-17 23:35:29 +0200

    added hiding note description

--- frontend/src/components/Note/Note.js
+++ frontend/src/components/Note/Note.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Note(props) {
+
+    const [showDesc, setShowDesc] = useState(false);
+    
+    const toggleDesc = () => {
+        console.log("klik");
+        setShowDesc(!showDesc);
+    };
+
     return ( 
         <div className="note">
-        <p>{props.title}</p>
+        <p onClick={toggleDesc}>{props.title}</p>
+        {showDesc && (
         <div className="description">{ props.body }</div>
+        )}
         <button>edytuj</button>
-        <button className="delete">usuń</button>
+        <button 
+            className="delete" 
+            onClick={() => props.onDelete(props.id)}>usuń</button>
     </div>
      );
 }
--- frontend/src/components/Notes/Notes.js
+++ frontend/src/components/Notes/Notes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Notes.css';
 import Note from '../Note/Note';
+import NewNote from '../NewNote/NewNote';
 
 class Notes extends React.Component {
     constructor(props) {
         super(props);
-        this.notes = [
+
+        this.state = {
+            notes : [
             {
                 id: '23232',
                 title: 'wykąpać psa',
@@ -16,22 +19,41 @@ class Notes extends React.Component {
                 title: 'zrobić zakupy',
                 body: 'kupić mleko maslo likier'
             }
-        ];
-    }
-    render() {
+        ]
+        };
 
+    }
+
+    deleteNote(id) {
+        console.log('usuwanie notatnik: ' + id);
+        const notes = [...this.state.notes]
+        .filter(note => note.id !== id)
+        this.setState({ notes });
+
+    }
+
+    addNote(note ) {
+        const notes = [...this.state.notes]
+    }
+
+    render() {
         return (
             <div>
                 <p>Moje notatki:</p>
 
-                {this.notes.map(note => (
-                    <Note title={note.title} body={note.body} id={note.id} />
+                <NewNote />
+
+                {this.state.notes.map(note => (
+                    <Note
+                        key = {note.id}
+                        title={note.title}
+                        body={note.body}
+                        id={note.id} 
+                        onDelete={(id)=>this.deleteNote(id)}/>
                 )
                 )}
-
             </div>
         )
-
     }
 }
 
